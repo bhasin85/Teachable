d77a9ac0515942c0d7944d026dc974ab7036b71d
Ankit Bhasin <bhasin85@gmail.com>
2020-06-23 19:40:19 +1000

    Working Solution

--- 3. Arrays/GasStation.py
+++ 3. Arrays/GasStation.py
@@ -1,31 +1,19 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-
-        for i in range(len(gas)):
-            j = i
-            tank = gas[j]
-            count = 0
-            #print("i:{} j:{} tank:{}".format(i,j,tank))
-            for k in range(len(gas)+1):
-                # loop detected
-                if j == i:
-                    count += 1
-                    
-                prev_j = j
-                if j+1 == len(gas):
-                    j = 0
-                else:
-                    j += 1
-                
-                # Out of budget
-                if tank <= 0 or count == 2 or tank < cost[prev_j]:
-                    break
-                    
-                ff = "{}-{}+{}".format(tank, cost[prev_j], gas[j])
-                tank = tank - cost[prev_j] + gas[j]
-                #print("loop i:{} j:{} tank {}={}".format(i,j,ff,tank))
-                
-            if count == 2: 
-                return i
-            
-        return -1
+        # If gas > cost, only then solution exist
+        if sum(gas) >= sum(cost):
+            start = 0
+            tank = 0
+            # Iterate the stations and start at 0
+            for i in range(len(gas)):
+                # Calculate tank at each station
+                tank = tank + gas[i] - cost[i]
+                #print("i:{} tank:{}".format(i, tank))
+                # If tank is negetive, reset tank and start from next station
+                if tank < 0:
+                    tank = 0
+                    start = i + 1
+                    #print("resetting i:{} tank:{}".format(i, tank))
+            return start
+        else:
+            return -1
