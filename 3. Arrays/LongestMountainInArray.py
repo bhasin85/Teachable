2132a32322a71ffe659c5db5567c51f6d02b0ee0
Ankit Bhasin <bhasin85@gmail.com>
2020-06-24 21:33:23 +1000

    Longest Mountain in Array

--- 3. Arrays/LongestMountainInArray.py
+++ 3. Arrays/LongestMountainInArray.py
@@ -1,32 +1,41 @@
-#           
-#    a       b
-# [2,1,4,7,3,2,5]
 class Solution:
     def longestMountain(self, A: List[int]) -> int:
+        print(len(A))
         longest = 0
-        i = 1
-        
-        if len(A) < 3:
-            return longest
-        
-        while i <= len(A)-2:
-            # Check for height
-            height = 1
-            while i-height>=0 and i+height<len(A):
-                left = i - height
-                right = i + height
 
-                if A[left] < A[left+1] and A[right] < A[right-1]:
-                    longest = max(longest, height)
-                    height += 1
-                    #print("true")
-                else:
-                    #print("false")
-                    break
+        # start from left and keep going till you find peak and than end of mountain
+        # update the longest length, if curr_longest > = 3
+        # start again till all the entries are traversed
+
+        curr_longest = 0
+        peak = False
+        for i in range(1, len(A)-1):
+            #print("i:{} A[i]:{} peak:{} curr_longest:{}".format(i, A[i], peak, curr_longest))
             
-            i += 1
-        
-        if longest>0:
-            return longest * 2 + 1
-        else:
-            return longest
+            if A[i-1] < A[i] < A[i+1]:
+                # going up
+                curr_longest = curr_longest + 1
+            elif A[i-1] < A[i] > A[i+1]:
+                # peak found
+                curr_longest = curr_longest + 1
+                peak = True
+            elif A[i-1] > A[i] > A[i+1]:
+                # going down
+                curr_longest = curr_longest + 1
+            else:
+                # update longest if peak was found
+                if peak:
+                    curr_longest = curr_longest + 2
+                    if curr_longest >= 3:
+                        longest = max(longest, curr_longest)
+                # reset
+                curr_longest = 0
+                peak = False  
+                
+        # update longest if peak was found
+        if peak:
+            curr_longest = curr_longest + 2
+            if curr_longest >= 3:
+                longest = max(longest, curr_longest)
+                        
+        return longest
