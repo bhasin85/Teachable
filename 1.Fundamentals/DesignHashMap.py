640529a5b070461764aae780278e9dc5b92923c2
Ankit Bhasin <bhasin85@gmail.com>
2020-06-28 15:50:39 +1000

    Update DesignHashMap.py

--- 1.Fundamentals/DesignHashMap.py
+++ 1.Fundamentals/DesignHashMap.py
@@ -10,35 +10,36 @@ class MyHashMap:
         """
         Initialize your data structure here.
         """
-        self.map = [Node("dummy", "dummy") for _ in range(10000)]
+        self.map = [Node(None, None) for _ in range(10000)]
 
-    def hash_func(self, key):
-        return key % 10000
+    def hashedIndex(self, key):
+        return key % 9999
     
     def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
         """
-        idx = self.hash_func(key)
-        prev = self.map[idx]
-        curr = prev.next
+        idx = self.hashedIndex(key)
+        curr = self.map[idx]
+        updated = False
         
-        while curr:
+        while curr.next:
+            curr = curr.next
             if curr.key == key:
                 curr.value = value
-                return
-            prev = curr
-            curr = curr.next
-        
-        prev.next = Node(key, value)
+                updated = True
+                break
+            
+        if not updated:
+            curr.next = Node(key, value)
+
 
     def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
-        idx = self.hash_func(key)
+        idx = self.hashedIndex(key)
         curr = self.map[idx]
-        
         while curr:
             if curr.key == key:
                 return curr.value
@@ -50,14 +51,14 @@ class MyHashMap:
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
-        idx = self.hash_func(key)
+        idx = self.hashedIndex(key)
         prev = self.map[idx]
         curr = prev.next
-        
         while curr:
             if curr.key == key:
-                prev.next = curr.next 
+                prev.next = curr.next
                 return
+            prev = curr
             curr = curr.next
 
 
